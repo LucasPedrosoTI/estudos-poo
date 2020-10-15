41c5aa83fb497d4e33cd8315ee6e3c83e3ae8d7c
lspt <lucas.pedroso@gft.com>
2020-10-15 14:24:22 -0300

    Exercício 1: Class Veiculo + métodos + atributos

---
+++ exercicio1/src/main/java/com/gft/exercicioUm/App.java
@@ -0,0 +1,44 @@
+package com.gft.exercicioUm;
+
+import com.gft.exercicioUm.Veiculo.Veiculo;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        Veiculo veiculo = new Veiculo();
+        veiculo.setCor("Preto");
+
+        veiculo.acelerar();
+        veiculo.acelerar();
+
+        System.out.println("Acelerou para " + veiculo.getVelocidade());
+
+        veiculo.abastecer(30);
+
+        System.out.println("Fuel " + veiculo.getLitrosCombustivel());
+
+        veiculo.abastecer(30);
+
+        System.out.println("2nd fuel " + veiculo.getLitrosCombustivel());
+
+        veiculo.frear();
+
+        System.out.println("Freou para " + veiculo.getVelocidade());
+
+        System.out.println("Cor anterior: " + veiculo.getCor());
+
+        veiculo.pintar("Vermelho");
+
+        System.out.println("Cor nova: " + veiculo.getCor());
+
+        System.out.println(veiculo.isLigado());
+        veiculo.ligar();
+        System.out.println(veiculo.isLigado());
+
+        veiculo.frear();
+        veiculo.desligar();
+        System.out.println(veiculo.isLigado());
+
+    }
+
+}
